12e4664959d8e24bcee051df49453841e4964bc2
Gilbert Solano <gilbertj.solano04@gmail.com>
2021-07-09 08:05:31 -0700

    Initial Commit

---
+++ NewTimeApp/NewTimeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewTimeAppApp.swift
+//  NewTimeApp
+//
+//  Created by Gilbert Solano on 7/9/21.
+//
+
+import SwiftUI
+
+@main
+struct NewTimeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
